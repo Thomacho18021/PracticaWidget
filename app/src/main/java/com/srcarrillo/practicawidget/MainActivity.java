acdba2cdd791390959552fbb4265b7bb303cf578
Thomas Carrillo <Thomascarrillogonzalez@gmail.com>
2020-09-09 12:18:16 -0300

    4 commit: Se corrigen errores de funcionalidad y verificacion de errores

--- app/src/main/java/com/srcarrillo/practicawidget/MainActivity.java
+++ app/src/main/java/com/srcarrillo/practicawidget/MainActivity.java
@@ -3,6 +3,8 @@ package com.srcarrillo.practicawidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     private RatingBar ratingBar;
     private Button btnEnviar;
     private boolean NombreOK = false;
+    private boolean RadioOk = false;
+    private String rbSelect;
+    private String cbSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,40 +67,81 @@ public class MainActivity extends AppCompatActivity {
                     cbNunca.setEnabled(true);
                     cbAVeces.setEnabled(true);
                     cbSiempre.setEnabled(true);
+                    rbSelect = "Si";
 
 
                 }else{
+                    cbNunca.setEnabled(false);
                     cbNunca.setChecked(false);
+                    cbAVeces.setEnabled(false);
                     cbAVeces.setChecked(false);
+                    cbSiempre.setEnabled(false);
                     cbSiempre.setChecked(false);
                     ratingBar.setRating(0);
                     ratingBar.setEnabled(false);
                     btnEnviar.setEnabled(true);
+                    rbSelect = "No";
 
-                }if (NombreOK == true){
+                }RadioOk=true;
+                if (NombreOK == true){
                     btnEnviar.setEnabled(true);
                 }
             }
         });
 
-        etNombre.setOnKeyListener(new View.OnKeyListener() {
+        etNombre.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                if (etNombre.getText().toString().length()<3){
-                    etNombre.setError("Nombre muy Corto");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() < 3){
+                    etNombre.setError("Nombre Corto, Mínimo 3 caracteres");
+                    NombreOK = false;
                     btnEnviar.setEnabled(false);
+
                 }else{
                     NombreOK = true;
+                    if (RadioOk == true){
+                        btnEnviar.setEnabled(true);
+
+                    }
 
                 }
-                return false;
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
 
     }
     public void Enviar (View view){
-        Toast.makeText(this, "Gracias por Colaborar", Toast.LENGTH_LONG).show();
-    }
-}
+        String nombre = etNombre.getText().toString().trim();
+
+        if(rbSelect == "Si"){
+            if (cbNunca.isChecked()==true){
+                cbSelect = "Nunca";
+                Toast.makeText(this, "Gracias "+nombre+"\n"+rbSelect + " te gusta Pepsi\n"+cbSelect+" Consumes Pepsi\n"+ratingBar.getRating()+" Estrellas", Toast.LENGTH_LONG).show();
+
+            }else if(cbAVeces.isChecked()==true){
+                cbSelect = "A veces";
+                Toast.makeText(this, "Gracias "+nombre+"\n"+rbSelect + " te gusta Pepsi\n"+cbSelect+" Consumes Pepsi\n"+ratingBar.getRating()+" Estrellas", Toast.LENGTH_LONG).show();
+
+            }else if(cbSiempre.isChecked()==true){
+                cbSelect = "Siempre";
+                Toast.makeText(this, "Gracias "+nombre+"\n"+rbSelect + " te gusta Pepsi\n"+cbSelect+" Consumes Pepsi\n"+ratingBar.getRating()+" Estrellas", Toast.LENGTH_LONG).show();
+
+            }else{
+                txtCheckBox.setError("No haz seleccionado una opcion");
+                return;
+            }
+        }else{
+            Toast.makeText(MainActivity.this, "Gracias por Colaborar", Toast.LENGTH_LONG ).show();
+        }
+}}
